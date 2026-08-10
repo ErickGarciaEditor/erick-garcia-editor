0e1fa270fef2a7022960e51fcb62531594a28e84
ErickGarciaEditor <garciasibipora@gmail.com>
2026-08-10 17:55:26 -0300

    Remove tradutor antigo do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { SafeImage } from './components/SafeImage';
 import { SafeVideo } from './components/SafeVideo';
-import { LanguageLayer } from './components/LanguageLayer';
 import { QuoteForm } from './components/QuoteForm';
 import { cases } from './data/cases';
 import { contact } from './data/contact';
@@ -769,7 +768,6 @@ export default function App() {
 
   return (
     <div className="site-shell">
-      <LanguageLayer />
       <header className="site-header">
         <div className="container header-row">
           <a className="brand" href="#inicio" aria-label="Erick Garcia">
